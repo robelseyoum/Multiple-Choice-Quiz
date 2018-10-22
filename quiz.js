a629657a9dc92f6cdf66ad84289762b9ade3e617
Robel <robelseyoum3@gmail.com>
2018-10-22 17:29:11 +0100

    adding files

---
+++ quiz.js
@@ -0,0 +1,77 @@
+/* 
+start
+quiz
+qImg
+question
+choiceA 
+counter
+timeGauge
+progress
+score
+ */
+
+//SELECTING ELEMENT BY ID
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+
+const qImage = document.getElementById("qImage"); 
+const question = document.getElementById("question");
+const counter = document.getElementById("counter");
+const btimeGauge = document.getElementById("btimeGauge");
+const timeGauge = document.getElementById("timeGauge");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceB = document.getElementById("C");
+
+const scoreCont = document.getElementById("scoreContainer");
+
+
+
+
+
+
+
+//CREATING THE QUESTIONS
+
+
+
+
+
+//CREATE SOME VARIABLES
+
+
+
+
+
+//RENDER QUESTIONS
+
+
+
+
+
+
+//START QUIZ
+
+
+
+
+//RENDER PROGRESS
+
+
+
+
+
+//COUNTER RENDER
+
+
+
+
+
+//CHECK THE ANSWER
+
+
+
+
+
+//answer is correct
